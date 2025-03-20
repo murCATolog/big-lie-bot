7bf7207613934e4251658f43a505d6b2abb3947c
murCATolog <murcatolog@gmail.com>
2025-03-20 12:43:51 +0200

    Refactored code to use OOP principles. Added new buttons for bot interaction.

--- bot.js
+++ bot.js
@@ -1,21 +1,56 @@
-//імпортуємо клас Telegraf із бібліотеки telegraf
-import { Telegraf } from "telegraf";
-// імпортуємо цитати з локального файлу quotes.json і кажемо NodeJS розпарсити його як обʼєкт
+// імпортуємо клас Telegraf із бібліотеки telegraf
+import { Telegraf, Markup } from "telegraf";
+// імпортуємо цитати з локального файлу quotes.json і NodeJS разопарсить його як обʼєкт
 import quotes from './quotes.json' with { type: 'json' };
-//створюємо бота з токену
-const bot = new Telegraf(/* BOT_TOKEN */);
-
-//налаштовуємо обробку команди /start
-bot.start((context) => {
-//отримуємо рандомний індекс
-    if(quotes.length > 0) {
-        const getRandomQuote = () => Math.floor(Math.random() * quotes.length);
-        let number = getRandomQuote();
-        context.reply(quotes[number].quote + '\n' + quotes[number].author);
-    } else {
-        context.reply('Упс! Щось пішло не так! 😢 \n Повідом мене про помилку: \n @murCATolog');
+// '7570011602:AAG9gpgzgg_MFxJBKVjFBhm99kG79_f9TTU');
+// створюємо клас бот
+class QuoteBot extends Telegraf {
+    constructor(token) {
+        super(token); // токен боту(метод успадкований з класу Telegraf)
+        this.active = true;    // стан боту
+        this.quotes = quotes;   // тут обʼєкт з цитатами
+        //кнопки бота
+        this.startKey = Markup.keyboard(['Запустити бота', 'Згенерувати цитату']).resize();
+        this.stopKey = Markup.keyboard(['Зупинити бота', 'Згенерувати цитату']).resize();
+        this.genQuoteKey = Markup.keyboard(['Згенерувати цитату']).resize();
     }
-});
+    // старт бота
+    startBot() {
+        this.start((context) => {   //метод start із бібліотеки Telegraf
+            context.reply(
+                'Доооооообрий вечір, хлопці!\nЯ буду генерувати Вам фрази широковідомих у вузьких колах шахраїв\n(і не тільки).\nЄ питання? Пиши: @murCATolog',
+                this.stopKey
+            );
+        });
+    };
+    // логіка кнопок
+    buttonsAction() {
+        this.hears('Зупинити бота', (context) => {
+            this.active = false;
+            context.reply('Бота зупинено.', this.startKey);
+        });
+        this.hears('Запустити бота', (context) => {
+            this.active = true;
+            context.reply('Бота запущено.', this.stopKey);
+        });
+        this.hears('Згенерувати цитату', (context) => {
+            context.reply(this.getRandomQuote(),  this.genQuoteKey);
+        });
 
-//запускаємо бота
-bot.launch();
+    };
+    // генеруємо рандомні цитати
+    getRandomQuote() {
+        if (this.quotes.length > 0) {
+            const number = Math.floor(Math.random() * this.quotes.length);
+            return `${this.quotes[number].quote}\n— ${this.quotes[number].author}`; 
+        } else {
+            return 'Упс! Чомусь цитата не завантажилась! 😢 \nПовідом мене про помилку: \n@murCATolog';
+        };
+    };
+};
+// створюємо екземпляр бота і запускаємо його
+const BigLiesBot = new QuoteBot('YOUR_NEW_TOKEN_HERE');
+
+BigLiesBot.startBot();
+BigLiesBot.buttonsAction();
+BigLiesBot.launch();
