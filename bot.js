f1c16c4e45c6d66f7d34c8e63e415ddc8671f064
murCATolog <murcatolog@gmail.com>
2025-03-21 17:45:47 +0200

    Add welcome image to bot start, refactor event handlers, and implement quote scheduling automation.

--- bot.js
+++ bot.js
@@ -9,40 +9,60 @@ class QuoteBot extends Telegraf {
         this.activeGen = true;    // стан активності автоматичних цитат
         this.quotes = quotes;   // тут обʼєкт з цитатами
         // кнопки бота
-        this.startKey = Markup.keyboard(['Згенерувати цитату! Прі мнє!', 'Запланувати цитату', 'Автогенерація цитат(OFF)']).resize();
-        this.stopKey = Markup.keyboard(['Згенерувати цитату! Прі мнє!', 'Запланувати цитату', 'Автогенерація цитат(ON)']).resize();
-        this.genQuote = Markup.keyboard(['Згенерувати цитату! Прі мнє!']).resize();
-        this.quoteAlertKey = Markup.keyboard(['Запланувати цитату']).resize();
-        this.hours = ['0:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'];
-        // обрати годину для генерації цитати
+        // назви кнопок
+        this.keysNames = {
+            generate: 'Згенерувати цитату. Прі мнє!',
+            plan: 'Запланувати цитату',
+            on: 'Включити автогенерацію цитат',
+            off: 'Вимкнути автогенерацію цитат',
+        };
+        // кнопки
+        this.genQuoteKey = Markup.keyboard([this.keysNames.generate]).resize();
+        this.quoteAlertKey = Markup.keyboard([this.keysNames.plan]).resize();
+        // масив з набором годин
+        this.hours = ['8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00'];
+        // конпка для вибору години для генерації цитати
         this.hoursKey = Markup.keyboard(this.hours).resize();
-        // логіка кнопок
-        this.hears('Згенерувати цитату! Прі мнє!', (context) => {
+        // викликаємо обробник подій
+        this.eventKeys();
+    };
+    // генерація меню в залежності від on/off
+    getButtonsMenu() {
+        const startKeys = Markup.keyboard([this.keysNames.generate, this.keysNames.plan, this.keysNames.on]).resize();
+        const stopKeys = Markup.keyboard([this.keysNames.generate, this.keysNames.plan, this.keysNames.off]).resize();
+        return this.activeGen ? stopKeys : startKeys;
+    };
+    // обробка всіх подій(окрім getButtonsMenu)
+    eventKeys() {
+        this.start(async (context) => {   //метод start із бібліотеки Telegraf
+            try{
+                await context.replyWithPhoto({ source: './img/main.jpg' });
+            }
+            catch(error) {
+                console.log(error);
+                context.reply('Упс! Чомусь картинка не завантажилась! 😢 \nПовідом мене про помилку: \n@murCATolog');
+            }
+            await context.reply(
+                'Доооооообрий вечір, хлопці!\nЯ буду генерувати Вам фрази широковідомих у вузьких колах шахраїв\n(і не тільки).\nТи можеш генерувати цитати одразу або ж\nналаштувати автоматичну генерацію раз на добу.\nЄ питання? Пиши: @murCATolog'
+            );
+        });
+        this.hears(this.keysNames.generate, (context) => {
             context.reply(this.getRandomQuote());
         });
-        this.hears('Автогенерація цитат(ON)', (context) => {
+        this.hears(this.keysNames.off, (context) => {
             this.activeGen = false;
-            context.reply('Головєшка ВИМКНУВ автоматичну генерацію цитат. Ти вчинив як п...', this.startKey);
+            context.reply('Головєшка ВИМКНУВ автоматичну генерацію цитат. Ти вчинив як п...', this.getButtonsMenu());
         });
-        this.hears('Автогенерація цитат(OFF)', (context) => {
+        this.hears(this.keysNames.on, (context) => {
             this.activeGen = true;
-            context.reply('Порядочно! Генерація цитат УВІМКНЕНА!',this.stopKey);
+            context.reply('Порядочно! Генерація цитат УВІМКНЕНА!',this.getButtonsMenu());
         });
-        this.hears('Запланувати цитату', (context) => {
-            context.reply('Малий, о котрій годині присилати цитату?', this.hoursKey);
+        this.hears(this.keysNames.plan, (context) => {
+            context.reply('Малой, о котрій годині присилати цитату?', this.hoursKey);
         });
         this.hears(this.hours, (context) => {
-            this.activeGen;
-            context.reply('Цитату заплановано.', this.stopKey);
-        });
-    };
-    // старт бота
-    startBot() {
-        this.start((context) => {   //метод start із бібліотеки Telegraf
-            context.reply(
-                'Доооооообрий вечір, хлопці!\nЯ буду генерувати Вам фрази широковідомих у вузьких колах шахраїв\n(і не тільки).\nТи можеш генерувати цитати одразу або ж\nналаштувати автоматичну генерацію раз на добу.\nЄ питання? Пиши: @murCATolog',
-                this.startKey
-            );
+            this.activeGen = true;
+            context.reply('Цитату заплановано.', this.getButtonsMenu());
         });
     };
     // генеруємо рандомні цитати
@@ -56,7 +76,6 @@ class QuoteBot extends Telegraf {
     };
 };
 // створюємо екземпляр бота і запускаємо його
-const BigLiesBot = new QuoteBot('YOUR_TOKEN!');
+const BigLiesBot = new QuoteBot('7570011602:AAG9gpgzgg_MFxJBKVjFBhm99kG79_f9TTU');
 
-BigLiesBot.startBot();
 BigLiesBot.launch();
